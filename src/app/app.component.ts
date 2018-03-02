d1484f820be068852688155c6259de68f94b27b2
zhaoyu <914888156@qq.com>
2018-03-02 17:19:37 +0800

    完成所有教程 ，除了 http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `
+    <h1>{{title}}</h1>
+    <nav>
+    <a routerLink = "/dashboard" routerLinkActive="active">Dashboard</a>
+    <a routerLink = "/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>  
+    `,
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent{
+    title = 'Tour of Heroes';
 }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component,OnInit } from '@angular/core';
+import {Hero} from './hero';
+
+import { Router} from '@angular/router';
+import {HeroService} from './hero.service';
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  providers:[HeroService]
+})
+
+export class HeroesComponent implements OnInit{ 
+  constructor(
+    private heroService:HeroService,
+    private router:Router
+  ){}
+  
+  heroes: Hero[];
+  selectedHero: Hero;
+  getHeroes():void{
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  };
+
+  onSelect(hero:Hero):void{
+    this.selectedHero = hero;
+  }
+  ngOnInit():void{
+    this.getHeroes();
+  }
+  gotoDetail(){
+    this.router.navigate(['/herodetail',this.selectedHero.id]);
+  }
+} 
