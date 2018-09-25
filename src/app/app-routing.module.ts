8d2501597f1bc4c38695f0e51b15ea58ffd9b0b2
zhaoyu <914888156@qq.com>
2018-09-25 13:53:37 +0800

    添加ngx-dnd 模块，还未使用

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule,Routes } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PanelComponent } from './panel/panel.component';
 
 const routes:Routes = [
     { path:'',redirectTo:'/dashboard',pathMatch:'full'},
     { path: 'dashboard',component:DashboardComponent},
     { path:'heroes',component:HeroesComponent },
-    { path:'herodetail/:id',component:HeroDetailComponent}
+    { path:'herodetail/:id',component:HeroDetailComponent},
+    { path:'panel',component:PanelComponent}
 ];
 @NgModule({
     imports:[RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { HttpModule } from '@angular/http'; 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-
+import { NgxDnDModule } from '@swimlane/ngx-dnd'; 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
@@ -15,19 +14,24 @@ import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PanelComponent } from './panel/panel.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule
+    AppRoutingModule,
+    NgxDnDModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
